d60cb43011bbb8321b15cc8cce4ed799620ed7d2
Piotr Woznicki <p.woznicki@SMART0135.local>
2021-09-28 18:11:06 +0200

    add dependencies

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    install_requires = [
+        "matplotlib",
+        "numpy",
+        "pyradiomics==3.0.1",
+        "nibabel==3.2.0",
+        "SimpleITK==2.0.2"
+        ]
+    )
+
