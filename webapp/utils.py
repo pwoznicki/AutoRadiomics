7c1bcfe640c5ec0d3c7be16a74aad96a1b48dd4c
Piotr Woznicki <piotrekwoznicki@gmail.com>
2021-11-10 16:55:19 +0100

    full workflow with feature extractiion for binary classification

--- webapp/utils.py
+++ webapp/utils.py
@@ -1,4 +1,8 @@
+import streamlit as st
+import pandas as pd
 import importlib.util
+import tkinter as tk
+from tkinter import filedialog
 
 # from https://github1s.com/jrieke/traingenerator/blob/HEAD/app/utils.py#L1-L177
 def import_from_file(module_name: str, filepath: str):
@@ -17,3 +21,27 @@ def import_from_file(module_name: str, filepath: str):
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
+
+
+def load_df(label):
+    uploaded_file = st.file_uploader(label)
+    if uploaded_file is None:
+        st.stop()
+    df = pd.read_csv(uploaded_file)
+    return df
+
+
+# from https://github.com/streamlit/streamlit/issues/1019
+def folder_picker(label="Select folder"):
+    root = tk.Tk()
+    root.withdraw()
+
+    root.wm_attributes("-topmost", 1)
+
+    # Add button
+    clicked = st.button(label=label)
+    if clicked:
+        dirname = st.text_input(
+            "Selected folder:", filedialog.askdirectory(master=root)
+        )
+    return dirname
