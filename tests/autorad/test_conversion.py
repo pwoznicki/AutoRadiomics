1a8ee7d2b47f981ddd860c7d4a7fdc9e8ed5ccb7
Piotr Woznicki <piotr.a.woznicki@gmail.com>
2022-11-27 21:02:14 +0100

    add shap and sklearn dependency

--- tests/autorad/test_conversion.py
+++ tests/autorad/test_conversion.py
@@ -2,10 +2,13 @@ import os
 import tempfile
 from pathlib import Path
 
+import pytest
+
 from autorad.config import config
 from autorad.utils import conversion
 
 
+@pytest.mark.skip(reason="requires external dependency")
 def test_dicom_to_nifti():
     dicom_dir = Path(config.TEST_DATA_DIR) / "DICOM"
     with tempfile.TemporaryDirectory() as tmp_dir:
